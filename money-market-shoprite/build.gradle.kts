3b4e72c734be35b46489bffdc79c4707dc0d7101
nishal morar <nishal.morar@angloamerican.com>
2024-03-15 12:14:47 +0200

    Add missing dependencies.
    Add first db model.

--- money-market-shoprite/build.gradle.kts
+++ money-market-shoprite/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.4"
 	kotlin("jvm") version "1.9.22"
 	kotlin("plugin.spring") version "1.9.22"
+	kotlin("plugin.jpa") version "1.9.22"
 }
 
 group = "com.example"
@@ -20,10 +21,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- money-market-shoprite/src/main/kotlin/com/example/moneymarketshoprite/Transaction.kt
+++ money-market-shoprite/src/main/kotlin/com/example/moneymarketshoprite/Transaction.kt
@@ -1,4 +1,15 @@
 package com.example.moneymarketshoprite
 
-class Transaction {
-}
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import org.springframework.data.annotation.Id
+
+@Entity(name = "transaction")
+data class TransactionEntity(
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        var id: Long?,
+        var amount: Double,
+        var description: String,
+        var currency: String)
